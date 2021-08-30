2bfb4d1685b0beb4f34f096d88f5a07d6b043a70
D-Morais <moraisdionata@gmail.com>
2021-08-30 18:19:01 -0300

    Atualização da api

---
+++ personapi/src/main/java/com/digitalinnovationone/personapi/exception/PersonNotFoundException.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.personapi.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonNotFoundException extends Exception{
+
+    public PersonNotFoundException(Long id){
+        super(String.format("Person with ID %s not found!", id));
+    }
+
+}
